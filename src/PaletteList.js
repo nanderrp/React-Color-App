82fcca00a32184ff46025b72fcf0db1870b3479e
Alexander Pierpoint <alexanderpierpoint@Rebeccas-MacBook-Pro-2.local>
2019-10-25 10:48:26 -0700

    added routes to palette list

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import MiniPalette from './MiniPalette';
 import { withStyles } from "@material-ui/styles";
+import {  } from "react-router-dom";
 
 
 class PaletteList extends Component {
+
+    goToPalette(id) {
+        this.props.history.push(`/palette/${id}` )
+    }
+
     render() {
         const { palettes, classes } = this.props;
         return (
             <div className={classes.root}>
                 <div className={classes.container}>
                     <nav className={classes.nav}>
-                        <h1>React Colors</h1>
+                        <h1>React Palette Master</h1>
                     </nav>
                     <div className={classes.palettes}>
                         {palettes.map(palette => (
-                        <MiniPalette {...palette}/>
+                        <MiniPalette {...palette} handleClick={() => this.goToPalette(palette.id)}/>
                         ))}
                     </div>
                 </div>
